daa1d9fc22efeb3da92b035fccacd150997d2871
menam elsobky <meenamelsobky@gmail.com>
2024-05-28 15:08:48 +0300

    first 2 projects

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1,num2;
+        string operation;
+        Console.WriteLine("Hello, welcome to the calculator");
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("please enter your first number");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("please enter your second number");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("what type of operation you need\n");
+        Console.WriteLine("for adding enter a" +
+            "\nfor substracting enter s" +
+            "\nfor multiblying enter m" +
+            "\nfor divisoin enter d");
+        operation = Console.ReadLine();
+        Console.WriteLine("--------------------------------");
+        switch (operation)
+        {
+            case "a":
+                Console.WriteLine($"Result of {num1} + {num2} is: {num1 + num2}");
+                break;
+            case "s":
+                Console.WriteLine($"Result of {num1} - {num2} is: {num1 - num2}");
+                break;
+            case "m":
+                Console.WriteLine($"Result of {num1} * {num2} is: {num1 * num2}");
+                break;
+            case "d":
+                if (num2 != 0)
+                {
+                    Console.WriteLine($"Result of {num1} / {num2} is: {num1 / (double)num2}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
+    }
+}
---
+++ simpleDiceGame/simpleDiceGame/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int playerRandomNumber,enemyRandomNumber,playerPoints=0 ,enemyPoints=0;
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) 
+        {
+            Console.WriteLine($"\nthe {i+1} game");
+            Console.WriteLine("press any key to roll the dice");
+            Console.ReadKey( true);
+            playerRandomNumber = random.Next(1,7);
+            Console.WriteLine($"\nyou rolled a {playerRandomNumber}");
+            Console.WriteLine(".....");
+            System.Threading.Thread.Sleep(1000);
+            enemyRandomNumber = random.Next(1,7);
+            Console.WriteLine($"your enemy rolled a {enemyRandomNumber}");
+            if (playerRandomNumber > enemyRandomNumber) { Console.WriteLine("you win"); playerPoints++; }
+            else if (enemyRandomNumber > playerRandomNumber) { Console.WriteLine("enemy win"); enemyPoints++; }
+            else Console.WriteLine("draw"); 
+        }
+        Console.WriteLine("\n\n\n.....");
+        System.Threading.Thread.Sleep(1000);
+        if(playerPoints>enemyPoints) { Console.WriteLine("you win the game"); }
+        else if (playerPoints == enemyPoints) { Console.WriteLine("draw"); }
+        else { Console.WriteLine("enemy won the game"); };
+        Console.ReadKey();
+    }
+
+}
