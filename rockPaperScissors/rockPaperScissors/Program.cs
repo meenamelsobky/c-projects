a4cd404fc0fd6db9c9d40c16ac40c6d30d6b1b90
menam elsobky <meenamelsobky@gmail.com>
2024-05-28 15:55:25 +0300

    to do list addedd

---
+++ rockPaperScissors/rockPaperScissors/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace rockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerScore = 0, enemyScore = 0;
+            Console.WriteLine("Welcome to Rock Paper Scissors game");
+
+            while (playerScore != 3 && enemyScore != 3)
+            {
+                Console.WriteLine($"Player score: {playerScore}. Enemy score: {enemyScore}");
+                Console.WriteLine("Please enter 'r' for rock, 'p' for paper, or 's' for scissors:");
+                string playerChoice = Console.ReadLine();
+
+                // Generate computer's choice randomly
+                Random random = new Random();
+                int randomNumber = random.Next(1, 4); // Generates a random number between 1 and 3
+                string enemyChoice = "";
+                switch (randomNumber)
+                {
+                    case 1:
+                        enemyChoice = "r";
+                        Console.WriteLine("Enemy chose: Rock");
+                        break;
+                    case 2:
+                        enemyChoice = "p";
+                        Console.WriteLine("Enemy chose: Paper");
+                        break;
+                    case 3:
+                        enemyChoice = "s";
+                        Console.WriteLine("Enemy chose: Scissors");
+                        break;
+                }
+
+                // Determine the winner
+                if (playerChoice == enemyChoice)
+                {
+                    Console.WriteLine("It's a tie!");
+                }
+                else if ((playerChoice == "r" && enemyChoice == "s") ||
+                         (playerChoice == "p" && enemyChoice == "r") ||
+                         (playerChoice == "s" && enemyChoice == "p"))
+                {
+                    Console.WriteLine("Player wins this round!");
+                    playerScore++;
+                }
+                else
+                {
+                    Console.WriteLine("Enemy wins this round!");
+                    enemyScore++;
+                }
+            }
+
+            // Determine the overall winner
+            if (playerScore == 3)
+            {
+                Console.WriteLine("Player wins the game!");
+            }
+            else
+            {
+                Console.WriteLine("Enemy wins the game!");
+            }
+        }
+    }
+}
---
+++ toDoList/toDoList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace toDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the to-do list program");
+            List<string> taskList = new List<string>();
+            string option = "";
+
+            while (option != "e")
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("Enter 1 to add a task to the list");
+                Console.WriteLine("Enter 2 to remove a task from the list");
+                Console.WriteLine("Enter 3 to view the task list");
+                Console.WriteLine("Enter 'e' to exit the program");
+
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the task you want to add:");
+                        string task = Console.ReadLine();
+                        taskList.Add(task);
+                        Console.WriteLine("Task added successfully!");
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter the task you want to remove:");
+                        string taskToRemove = Console.ReadLine();
+                        if (taskList.Remove(taskToRemove))
+                        {
+                            Console.WriteLine("Task removed successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Task not found in the list!");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Task list:");
+                        foreach (string t in taskList)
+                        {
+                            Console.WriteLine("- " + t);
+                        }
+                        break;
+                    case "e":
+                        Console.WriteLine("Exiting the program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option! Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
